d17d80f0bc4214173d520954095b5c051ee2179d
furebo <furebodidace582@gmail.com>
2020-11-12 12:46:03 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyCZow2yx0WZ-pTe_Bos_2aajtDr_awiKmU",
+    authDomain: "react-auth-2c848.firebaseapp.com",
+    databaseURL: "https://react-auth-2c848.firebaseio.com",
+    projectId: "react-auth-2c848",
+    storageBucket: "react-auth-2c848.appspot.com",
+    messagingSenderId: "1093297833287",
+    appId: "1:1093297833287:web:29ebb548049b7ee4143862",
+    measurementId: "G-X2N19N27C4"
+
+})
+//const db = firebaseApp.firestore();
+const fire = firebaseApp;
+
+export default fire;
+
+//export default db;
